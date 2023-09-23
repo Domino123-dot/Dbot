857d8a3cfb04ff2fb9b2cec1bc0782cadd7769b4
Domino123-dot <pl.kubakot@Gmail.com>
2023-09-23 16:12:21 +0200

    added env variables

--- bot.js
+++ bot.js
@@ -1,60 +1,43 @@
 const discord = require("discord.js");
 const { Intents } = require("discord.js");
+require('dotenv').config();
 const client = new discord.Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
-
-client.on("message", async (message) => {
-setInterval(checkForUpdates , 500);
+client.on("ready", () => {
+  setInterval(checkForUpdates, 500);
+  
   function checkForUpdates() {
-  var comm = [];
-  const axios = require("axios");
-  var to_clear = false;
-  axios
-    .get("https://back-end.kanaquiz.online/api/discordBot/")
-    .then((response) => {
-
-      try {
-        for (var i = 0; i < response.data.length; i++) {
-          
-          comm.unshift(response.data[i].info);
-         
-      
-    }
-     
-        if (message.content === "wlacz") {
-          if (to_clear === true) {
-            to_clear = false;
-          }
-          else {
- 
-         
-            if (comm.length != 0) {
-              client.channels.cache
-                .get("1149746132587663494")
-                .send('@everyone' + comm);
-                axios
-                .delete("https://back-end.kanaquiz.online/api/discordBotDetails/1")
-                .then(() => console.log("nice!"));
-                comm = [];
-            
-            }
-           
-        }
-
-       
-      } } catch (error) {
-        console.error("An error occurred:", error);
-      }
     
-    });
-  }
+    var comm = [];
+    const axios = require("axios");
 
+    axios
+      .get("https://back-end.kanaquiz.online/api/discordBot/")
+      .then((response) => {
+        try {
+          for (var i = 0; i < response.data.length; i++) {
+            comm.unshift(response.data[i].info);
+            console.log(comm);
+          }
+
+          if (comm.length != 0) {
+            
+            client.channels.cache
+              .get("1148987884309852291")
+              .send("@everyone" + comm);
+            axios
+              .delete(
+                "https://back-end.kanaquiz.online/api/discordBotDetails/1"
+              )
+              .then(() => console.log("nice!"));
+            comm = [];
+          }
+        } catch (error) {
+          console.error("An error occurred:", error);
+        }
+      });
+  }
 });
 
-client.login(
-  "MTE0Nzg3NTUzMDI2NDMwMTU4OA.GhUYGZ.FPK7rVWBmC1YJYSng8SE5NnjKDlvq3auawzj18"
-);
-
-
-
+client.login(process.env.TOKEN);
